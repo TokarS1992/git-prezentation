548b737130c8e9278afecf6ac5b036f6aafebb41
Sergey Tokar <sergey.tokar@stripo.email>
2021-07-20 14:32:49 +0300

    Add new handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'git-presentation';
   newValue = false;
+  public newHandler() {
+    console.log('some changes');
+  }
 }
 
